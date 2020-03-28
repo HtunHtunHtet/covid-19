572331d773631e0fcdbedf3d397b9e25c93ee5dc
htunhtunhtet <ryanhhh91@gmail.com>
2020-03-28 12:44:22 +0800

    update case

--- assets/js/home.js
+++ assets/js/home.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
       maxZoom: 28,
-      attribution: 'Develop with ❤️ by <a href="https://www.linkedin.com/in/htun-htun-htet-75464299/" target="_blank">Htun Htun Htet</a> | Last update as 2020-03-27',
+      attribution: 'Develop with ❤️ by <a href="https://www.linkedin.com/in/htun-htun-htet-75464299/" target="_blank">Htun Htun Htet</a> | Last update as 2020-03-28',
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1
@@ -124,6 +124,65 @@ $(document).ready(function(){
 
    drawMarker(L,mymap,case5Detail)
 
+   /** Case 6 **/
+   let case6Detail= {
+      'caseNum'     : '6',
+      'confirmTime' : '2020-03-26 01:00:00',
+      'gender'      : 'Male',
+      'age'         : '29',
+      'from'        : 'United Kingdom, England',
+      'visited'     : 'Wai Bar Gi Hospital, Yangon, Myanmar (Burma)',
+      'currentTreatmentPlace': 'Wai Bar Gi Hospital, Yangon, Myanmar (Burma)',
+      'startTreatment'   : '2020-03-26',
+      'sourceLink'  : 'https://www.facebook.com/MinistryOfHealthAndSportsMyanmar/photos/pcb.3281142441915038/3281134091915873/?type=3&theater',
+      'sourceLabel' : 'Ministry of Health and Sports, Myanmar',
+      'lat'         : '16.920784',
+      'lng'         : '96.156547',
+      'fromLat'     : '62.066858',
+      'fromLng'      : '-6.700055'
+   }
+
+   drawMarker(L,mymap,case6Detail)
+
+   /** Case 7 **/
+   let case7Detail= {
+      'caseNum'     : '7',
+      'confirmTime' : '2020-03-26 01:00:00',
+      'gender'      : 'Female',
+      'age'         : '58',
+      'from'        : 'Switzerland, Geneve',
+      'visited'     : 'Naypyitaw Airport',
+      'currentTreatmentPlace': '1000 bedded Naypyitaw General Hospital, Naypyitaw',
+      'startTreatment'   : '2020-03-26',
+      'sourceLink'  : 'https://www.facebook.com/MinistryOfHealthAndSportsMyanmar/photos/pcb.3281142441915038/3281134091915873/?type=3&theater',
+      'sourceLabel' : 'Ministry of Health and Sports, Myanmar',
+      'lat'         : '19.743111',
+      'lng'         : '96.100803',
+      'fromLat'     : '46.203242',
+      'fromLng'      : '6.144946'
+   }
+
+   drawMarker(L,mymap,case7Detail)
+
+   /** Case 7 **/
+   let case8Detail= {
+      'caseNum'     : '8',
+      'confirmTime' : '2020-03-26 01:00:00',
+      'gender'      : 'Female',
+      'age'         : '60',
+      'from'        : 'Local Transmission',
+      'visited'     : 'Unknown (Local Tramsmission)',
+      'currentTreatmentPlace': 'Yangon General Hospital',
+      'startTreatment'   : '2020-03-26',
+      'sourceLink'  : 'https://www.facebook.com/MinistryOfHealthAndSportsMyanmar/photos/pcb.3281142441915038/3281134091915873/?type=3&theater',
+      'sourceLabel' : 'Ministry of Health and Sports, Myanmar',
+      'lat'         : '16.781626',
+      'lng'         : '96.144727',
+      'fromLat'     : 'null',
+      'fromLng'      : 'null'
+   }
+
+   drawMarker(L,mymap,case8Detail);
 });
 
 
@@ -158,15 +217,21 @@ function drawMarker(L,mymap,details) {
    L.marker([`${lat}`, `${lng}`], {icon: confirm}).addTo(mymap)
        .bindPopup(description);
 
-   let polylineLatLngs = [
-      [`${fromLat}`, `${fromLng}`],
-      [`${lat}`, `${lng}`]
-   ]
-
-   let polyline = L.polyline(polylineLatLngs, {color: 'red'}).addTo(mymap);
 
 
-   polyline.setStyle({
-      color: '#ff763b'
-   });
+   if (fromLng !== 'null' && fromLng !== null ){
+      let polylineLatLngs = [
+         [`${fromLat}`, `${fromLng}`],
+         [`${lat}`, `${lng}`]
+      ]
+
+      let polyline = L.polyline(polylineLatLngs, {color: 'red'}).addTo(mymap);
+
+
+      polyline.setStyle({
+         color: '#ff763b'
+      });
+   }
+
+
 }
